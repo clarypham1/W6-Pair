ec575cfe1827664585ea182272797b5b918a6343
Saga <sagalehtonen@gmail.com>
2026-09-22 15:09:11 +0300

    [iter5] Final Testing ...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
-Hello
+# Iteration 5:
+  
+  ## Did you need to make any changes to the tour-related controller functions? Why or why not?
+  ### No, we did not give any 'info', so it did not know what to create, delete getbyId.. etc. (also no changes)
+
+  
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const validator = require('validator');
 
 // Generate JWT
 const generateToken = (_id) => {
@@ -14,6 +15,13 @@ const generateToken = (_id) => {
 const signupUser = async (req, res) => {
   const { name, email, password, phone_number, gender, date_of_birth, membership_status } = req.body;
 
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    return res.status(400).json({ error: "Password is too weak" });
+  }
   try {
     const user = await User.signup(name, email, password, phone_number, gender, date_of_birth, membership_status);
 
@@ -63,4 +71,3 @@ module.exports = {
   loginUser,
   getMe,
 };
-
